e2263831ff41382c9f5518027609aa6d3457be42
jihye <bnm1354@gmail.com>
2024-02-13 09:51:44 +0900

    [ADD] 리액트 초기파일

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
+import {Home, SignIn, SignUp, NotFound, SelectPlant} from './pages'
+import NotAuthLayout from './components/layout/NotAuthLayout';
+import AuthLayout from './components/layout/AuthLayout';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+      <Routes>
+        <Route element={<AuthLayout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/selectPlant" element={<SelectPlant />} />
+        </Route>
+        <Route element={<NotAuthLayout />}>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/" element={<SignUp />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
